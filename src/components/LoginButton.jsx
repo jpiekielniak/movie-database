2f612e5f120a4f8309c4d0980a2f033e50066886
jpiekielniak <35228@student.pwsztar.edu.pl>
2023-11-19 12:10:15 +0100

    add : login component

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/login.css";
+
+const LoginButton = () => {
+  return (
+    <button className="login-button">
+      Zaloguj się 
+    </button>
+  );
+};
+
+export default LoginButton;
