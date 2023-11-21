72ecbd67f67a966b523e7cbdf0bcc2aa837345a0
jpiekielniak <35228@student.pwsztar.edu.pl>
2023-11-21 12:47:20 +0100

    refactor : footer

--- src/components/Footer/SocialMediaIcons.tsx
+++ src/components/Footer/SocialMediaIcons.tsx
@@ -1,26 +1,16 @@
-import { ActionIcon, rem } from "@mantine/core";
 import { useSocialMediaLinks } from "./hooks/useSocialMediaLinks";
+import SocialMedia from "./SocialMedia";
 
 export default function SocialMediaIcons() {
   const socialMediaLinks = useSocialMediaLinks();
 
   return (
-    <div>
-      {socialMediaLinks.map((item, index) => {
-        const Icon = item.icon;
-        return (
-          <a href={item.link} key={index} target="_blank" rel="noopener noreferrer">
-            <ActionIcon
-              size="lg"
-              color="black"
-              variant="subtle"
-              style={{ width: rem(25), height: rem(25) }}
-            >
-              <Icon stroke={1.5} />
-            </ActionIcon>
-          </a>
-        );
-      })}
+    <div style={{display: 'flex'}}>
+      {socialMediaLinks.map((media, index) => (
+        <div key={`${index}_${media.link}`} style={{ marginRight: '10px' }}>
+        <SocialMedia {...media} />
+        </div>
+      ))}
     </div>
   );
 }
