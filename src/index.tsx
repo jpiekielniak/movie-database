797e9c3e036f4180dea44f6db981428ff4676953
JPiekielniak <jakubpiekielniak123@gmail.com>
2023-12-20 18:09:38 +0100

    refactor: notFound component

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,38 @@
 import ReactDOM from "react-dom/client";
 import {
-  BrowserRouter,
-  Routes,
-  Route
+    BrowserRouter,
+    Routes,
+    Route
 } from "react-router-dom";
 import "./index.css";
 import App from "./containers/App";
-import { MantineProvider } from "@mantine/core";
+import {MantineProvider} from "@mantine/core";
 import "@mantine/core/styles.css";
 import SignInLayout from "./components/auth/SignIn/SignInLayout";
 import SignUpLayout from "./components/auth/signUp/SignUpLayout";
 import MovieDetails from "./components/MovieCard/MovieDetails";
-import NotFound  from "./components/notFound/NotFound";
-import Footer from "./components/footer/Footer";
+import NotFound from "./components/NotFound/NotFound";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import AddFilmLayout from "./components/addFilm/AddFilmLayout";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+    document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <MantineProvider>
-    <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/"  element={<App/>} />
-      <Route path="*" element={<NotFound/>} />
-      <Route path="/sign-in" element={<SignInLayout/>} />
-      <Route path="/sign-up" element={<SignUpLayout/>} />
-      <Route path="/details/:title" element={<MovieDetails />} />
-      <Route path="/add" element={<AddFilmLayout />} />
-    </Routes>
-    <Footer />
-    </BrowserRouter>
-  </MantineProvider>
+    <MantineProvider>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="*" element={<NotFound/>}/>
+                <Route path="/sign-in" element={<SignInLayout/>}/>
+                <Route path="/sign-up" element={<SignUpLayout/>}/>
+                <Route path="/details/:title" element={<MovieDetails/>}/>
+                <Route path="/add" element={<AddFilmLayout/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    </MantineProvider>
 );
