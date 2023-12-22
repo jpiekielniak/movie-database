6865c95f8e5fa90551248b91d97718530296a84b
JPiekielniak <jakubpiekielniak123@gmail.com>
2023-12-22 11:30:40 +0100

    add : sign up api

--- src/components/auth/signUp/SignUpLayout.tsx
+++ src/components/auth/signUp/SignUpLayout.tsx
@@ -1,83 +1,85 @@
 import React from "react";
-import { Container, TextInput, Button } from "@mantine/core";
+import {Container, TextInput, Button} from "@mantine/core";
 import styles from "./styles/signUpLayout.module.css";
-import { IconLogin2 } from "@tabler/icons-react";
-import { useSignUpData } from "./hooks/useSignUpData";
+import {IconLogin2} from "@tabler/icons-react";
 import SignInLink from "./SignInLink";
+import {useSignUpSubmit} from "./hooks/useSignUpSubmit";
+import {TSignUpForm} from "./types/SignUp";
+import useFormInput from "./hooks/useFormInput";
 
-const SignInLayout: React.FC = () => {
-  const [formData, handleChange] = useSignUpData();
+const SignUpLayout: React.FC = () => {
+    const name = useFormInput('');
+    const email = useFormInput('');
+    const password = useFormInput('');
+    const {handleSubmit} = useSignUpSubmit();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(formData);
-    // Send to API
-  };
+    const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-  return (
-    <Container className={styles.container}>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.description}>Rejestracja</div>
+        const formData: TSignUpForm = {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+        };
 
-        <div className={styles.input}>
-          <TextInput
-            required
-            id="login"
-            placeholder="Login"
-            value={formData.login}
-            onChange={handleChange}
-          />
-        </div>
+        try {
+            await handleSubmit(formData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-        <div className={styles.input}>
-          <TextInput
-            required
-            id="nick"
-            type="text"
-            placeholder="Nazwa"
-            value={formData.nick}
-            onChange={handleChange}
-          />
-        </div>
+    return (
+        <Container className={styles.container}>
+            <form className={styles.form} onSubmit={handleFormSubmit}>
+                <div className={styles.input}>
+                    <TextInput
+                        required
+                        id="name"
+                        type="text"
+                        placeholder="Nazwa"
+                        value={name.value}
+                        onChange={name.onChange}
+                    />
+                </div>
 
-        <div className={styles.input}>
-          <TextInput
-            required
-            id="email"
-            type="email"
-            placeholder="E-mail"
-            value={formData.email}
-            onChange={handleChange}
-          />
+                <div className={styles.input}>
+                    <TextInput
+                        required
+                        id="email"
+                        type="email"
+                        placeholder="E-mail"
+                        value={email.value}
+                        onChange={email.onChange}
+                    />
 
-          <div className={styles.input}>
-            <TextInput
-              required
-              id="password"
-              type="password"
-              placeholder="Hasło"
-              value={formData.password}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
+                    <div className={styles.input}>
+                        <TextInput
+                            required
+                            id="password"
+                            type="password"
+                            placeholder="Hasło"
+                            value={password.value}
+                            onChange={password.onChange}
+                        />
+                    </div>
+                </div>
 
-        <Button
-          type="submit"
-          fullWidth
-          variant="gradient"
-          gradient={{ from: "blue", to: "cyan" }}
-          className={styles.btn}
-        >
-          Zarejestruj się <IconLogin2 />
-        </Button>
-
-        <div className={styles.textCenter}>
-          <SignInLink />
-        </div>
-      </form>
-    </Container>
-  );
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="gradient"
+                    gradient={{from: "blue", to: "cyan"}}
+                    className={styles.btn}
+                >
+                    Zarejestruj się <IconLogin2/>
+                </Button>
+                <div className={styles.textCenter}>
+                    <SignInLink/>
+                </div>
+            </form>
+        </Container>
+    );
 };
 
-export default SignInLayout;
+export default SignUpLayout;
