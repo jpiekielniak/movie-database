48405d5c744f8d3bb68d91b2817e71d69c3334ac
JPiekielniak <jakubpiekielniak123@gmail.com>
2023-12-22 10:48:12 +0100

    refactor: header buttons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,33 @@
 import React from "react";
-import { Container, Image } from "@mantine/core";
-import { Link } from "react-router-dom";
-import SignInButton from "./SignInButton";
+import {Container, Image} from "@mantine/core";
+import {Link} from "react-router-dom";
 import styles from "./styles/header.module.css";
+import useHeaderButtons from "./hooks/useHeaderButtons";
 
 const Header: React.FC = React.memo(() => {
-
-  return (
-    <div className={styles.headerContainer}>
-      <Container>
-        <div className={styles.flexContainer}>
-          <div>
-            <Link to="/">
-              <Image
-                src="movie_club.png"
-                alt="Movie Logo"
-                width={120}
-                height={50}
-                className={styles.image}
-              />
-            </Link>
-          </div>
-          <div className={styles.rightContent}>
-            <SignInButton />
-          </div>
+    const buttons = useHeaderButtons();
+    return (
+        <div className={styles.headerContainer}>
+            <Container>
+                <div className={styles.flexContainer}>
+                    <div>
+                        <Link to="/">
+                            <Image
+                                src="movie_club.png"
+                                alt="Movie Logo"
+                                width={120}
+                                height={50}
+                                className={styles.image}
+                            />
+                        </Link>
+                    </div>
+                    <div className={styles.rightContent}>
+                        {buttons}
+                    </div>
+                </div>
+            </Container>
         </div>
-      </Container>
-    </div>
-  );
+    );
 });
 
 export default Header;
