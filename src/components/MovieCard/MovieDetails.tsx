8a997c8bb138d2ce43dce710486a5abc43c51c01
JPiekielniak <jakubpiekielniak123@gmail.com>
2024-01-10 11:08:05 +0100

    add : star rate

--- src/components/MovieCard/MovieDetails.tsx
+++ src/components/MovieCard/MovieDetails.tsx
@@ -1,71 +1,82 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom';
 import styles from './styles/movieDetails.module.css';
-import { MovieDetailsProps } from './types/MovieDetailsProps';
-import { UUID } from 'node:crypto';
+import {MovieDetailsProps} from './types/MovieDetailsProps';
+import {UUID} from 'node:crypto';
 import useFetchMovie from './hooks/useFetchMovie';
 import useDeleteMovie from "./hooks/useDeleteMovie";
 import useDecodeToken from "./hooks/useDecodeToken";
+import useStarRating from "./hooks/useStarRating";
 
 const MovieDetails: React.FC<MovieDetailsProps> = () => {
-  const { movieId } = useParams<{ movieId: UUID }>();
-  const [isAdmin, setIsAdmin] = useState(false);
-  const movie = useFetchMovie(movieId);
-  const token = localStorage.getItem('token');
-  const decodedToken: any = useDecodeToken(token);
-  const [showConfirmation, setShowConfirmation] = useState(false);
-  const [isDeleteButtonVisible, setIsDeleteButtonVisible] = useState(true);
-  const deleteMovie = useDeleteMovie();
+    const {movieId} = useParams<{ movieId: UUID }>();
+    const [isAdmin, setIsAdmin] = useState(false);
+    const movie = useFetchMovie(movieId);
+    const token = localStorage.getItem('token');
+    const decodedToken: any = useDecodeToken(token);
+    const [showConfirmation, setShowConfirmation] = useState(false);
+    const [isDeleteButtonVisible, setIsDeleteButtonVisible] = useState(true);
+    const deleteMovie = useDeleteMovie();
+    const stars = useStarRating(movie && movie.rate);
 
-  useEffect(() => {
-    if (decodedToken && decodedToken.role === 'admin') {
-      setIsAdmin(true);
-    }
-  }, [decodedToken]);
 
-  const handleDeleteMovie = () => {
-    setShowConfirmation(true);
-    setIsDeleteButtonVisible(false);
-  };
+    useEffect(() => {
 
-  const confirmDelete = () => {
-    deleteMovie(movieId, token).then(() => {
-        window.location.href = '/';
-    });
-    setShowConfirmation(false);
-  };
+        if (decodedToken && decodedToken.role === 'admin') {
+            setIsAdmin(true);
+        }
+    }, [decodedToken]);
 
-  const cancelDelete = () => {
-    setShowConfirmation(false);
-    setIsDeleteButtonVisible(true);
-  };
+    const handleDeleteMovie = () => {
+        setShowConfirmation(true);
+        setIsDeleteButtonVisible(false);
+    };
 
-  return (
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <img src={movie?.image} alt={movie?.title} className={styles.image} />
-          </div>
-          <div className={styles.details}>
-            <h2 className={styles.title}>{movie ? movie.title : 'Ładuję twoją zawartość :)'}</h2>
-            <p className={styles.description}>{movie ? movie.content : 'Ładuję twoją zawartość :)'}</p>
-            {isAdmin && isDeleteButtonVisible && (
-                <button onClick={handleDeleteMovie} className={`${styles.deleteButton} ${styles.redButton}`}>
-                  Usuń film
-                </button>
-            )}
-            {showConfirmation && (
-                <div className={styles.confirmationBox}>
-                  <p>Czy na pewno chcesz usunąć ten film?</p>
-                  <button onClick={confirmDelete} className={styles.confirmButton}>Tak</button>
-                  <button onClick={cancelDelete} className={styles.cancelButton}>Anuluj</button>
+    const confirmDelete = () => {
+        deleteMovie(movieId, token).then(() => {
+            window.location.href = '/';
+        });
+        setShowConfirmation(false);
+    };
+
+    const cancelDelete = () => {
+        setShowConfirmation(false);
+        setIsDeleteButtonVisible(true);
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.imageContainer}>
+                    <img src={movie?.image} alt={movie?.title} className={styles.image}/>
                 </div>
-            )}
-            {/*<div className={styles.rating}>{stars}</div>*/}
-          </div>
+
+
+                <div className={styles.details}>
+                    <h2 className={styles.title}>{movie ? movie.title : 'Ładuję twoją zawartość :)'}</h2>
+                    <p className={styles.description}>{movie ? movie.content : 'Ładuję twoją zawartość :)'}</p>
+
+                    <div className={styles.stars}>
+                        {stars}
+                    </div>
+
+                    {isAdmin && isDeleteButtonVisible && (
+                        <button onClick={handleDeleteMovie} className={`${styles.deleteButton} ${styles.redButton}`}>
+                            Usuń film
+                        </button>
+                    )}
+                    {showConfirmation && (
+                        <div className={styles.confirmationBox}>
+                            <p>Czy na pewno chcesz usunąć ten film?</p>
+                            <button onClick={confirmDelete} className={styles.confirmButton}>Tak</button>
+                            <button onClick={cancelDelete} className={styles.cancelButton}>Anuluj</button>
+                        </div>
+                    )}
+
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default MovieDetails;
