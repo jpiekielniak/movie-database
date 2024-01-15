a39fca145ca5370e37bd46b5465b0e3cb524bbbf
JPiekielniak <jakubpiekielniak123@gmail.com>
2024-01-15 18:40:45 +0100

    add : paginate

--- src/components/MovieCard/MovieList.tsx
+++ src/components/MovieCard/MovieList.tsx
@@ -1,35 +1,68 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import MovieCard from "./MovieCard";
 import styles from "./styles/movieList.module.css";
-import { Link } from "react-router-dom";
-import { MovieListProps } from "./types/MovieListProps";
+import {Link} from "react-router-dom";
+import {MovieListProps} from "./types/MovieListProps";
 import axios from "axios";
-import { TMovie } from "./types/Movie";
+import {TMovie} from "./types/Movie";
+import ReactPaginate from 'react-paginate';
 
-const MovieList: React.FC<MovieListProps> = ({ searchTerm }) => {
-  const [movies, setMovies] = useState<TMovie[]>([]);
+function MovieList(props: MovieListProps) {
+    const [movies, setMovies] = useState<TMovie[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const moviesPerPage = 8;
 
-  useEffect(() => {
-    axios.get('https://at.usermd.net/api/movies')
-        .then(res => setMovies(res.data))
-        .catch(err => console.log(err));
-  }, []);
+    useEffect(() => {
+        axios.get('https://at.usermd.net/api/movies')
+            .then(res => setMovies(res.data))
+            .catch(err => console.log(err));
+    }, []);
 
-  const filteredFilms = movies.filter(film =>
-      film.title.toLowerCase().includes((searchTerm || '').toLowerCase())
-  );
+    const {searchTerm} = props;
 
-  const containerClass = filteredFilms.length === 1 ? `${styles.container} ${styles.singleMovie}` : styles.container;
+    const filteredFilms = movies.filter(film =>
+        film.title && film.title.toLowerCase().includes((searchTerm || '').toLowerCase())
+    );
 
-  return (
-      <div className={containerClass}>
-        {filteredFilms.map((film, index) => (
-            <Link to={`/details/${film.id}`} key={`${index}`}>
-              <MovieCard {...film} />
-            </Link>
-        ))}
-      </div>
-  );
-};
+    const pageCount = Math.ceil(filteredFilms.length / moviesPerPage);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const offset = (currentPage - 1) * moviesPerPage;
+
+    const currentMovies = filteredFilms.slice(offset, offset + moviesPerPage);
+
+    const containerClass = filteredFilms.length === 1 ? `${styles.container} ${styles.singleMovie}` : styles.container;
+
+    return (
+        <div>
+            {pageCount > 1 && (
+                <div>
+                    <ReactPaginate
+                        activeClassName={styles.active}
+                        breakLabel="..."
+                        containerClassName={styles.pagination}
+                        onPageChange={(e) => handlePageChange(e.selected + 1)}
+                        nextLabel={'Następna'}
+                        previousLabel={'Poprzednia'}
+                        pageCount={pageCount}
+                        previousClassName={styles.previous}
+                        nextClassName={styles.next}
+                    />
+                </div>
+            )}
+
+            <div className={containerClass}>
+                {currentMovies.map((film, index) => (
+                    <Link to={`/details/${film.id}`} key={`${index}`}>
+                        <MovieCard {...film} />
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
 
 export default MovieList;
